ecc6b493798c51b59eb3ee280bec01536c8a0dca
J_Storm <liuxianyou6@sina.com>
2018-07-31 21:02:05 +0800

    增加此自定义的sessionListener

--- src/main/java/com/ssm/demo/listener/DemoRequestListener.java
+++ src/main/java/com/ssm/demo/listener/DemoRequestListener.java
@@ -14,6 +14,14 @@ import java.util.Set;
 
 /**
  * 自定义requestListener
+ *
+ * post提交产生乱码的原因：
+ * post方式提交的数据是在请求体中，request对象接收到数据之后，放入request的缓冲区中。缓冲区就有编码（默认ISO-8859-1:不支持中文）.
+ * 解决方案：将request的缓冲区的编码修改了即可。request.setCharacterEncoding("UTF-8");
+ *
+ * get提交产生乱码的原因:get方式提交的数据在请求行的url后面，地址栏上其实就已经进行一次URL的编码了.
+ * 解决方案:将存入到request缓冲区中的值以ISO-8859-1的方式获取到，以UTF-8的方式进行解码。
+ * 例如:String value = new String(name.getBytes("ISO-8859-1"),"UTF-8");
  */
 public class DemoRequestListener implements ServletRequestListener {
 
@@ -21,6 +29,7 @@ public class DemoRequestListener implements ServletRequestListener {
 
     @Override
     public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
+        logger.info("request 销毁了");
     }
 
     @Override
---
+++ src/main/java/com/ssm/demo/listener/DemoSessionListener.java
@@ -0,0 +1,40 @@
+package com.ssm.demo.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * 自定义httpSessionListener
+ */
+public class DemoSessionListener implements HttpSessionListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoSessionListener.class);
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+
+        HttpSession httpSession = httpSessionEvent.getSession();
+
+        logger.info("sessionId:" + httpSession.getId());
+
+        long lastAccessedTime = httpSession.getLastAccessedTime();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        logger.info("lastAccessedTime:" + simpleDateFormat.format(new Date(lastAccessedTime)));
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        logger.info("session 销毁了");
+    }
+}
